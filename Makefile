47504c685e4066b9cb38d51239347c5986756d5f
plgch <plagache@protonmail.com>
2025-09-27 22:41:13 +0200

    go for optimizer

---
+++ Makefile
@@ -0,0 +1,58 @@
+PYTHON_VERSION = 3.12
+VENV = .venv
+BIN = ${VENV}/bin
+PYTHON = ${BIN}/python
+ACTIVATE = ${BIN}/activate
+
+EXAMPLES = examples
+
+PROGRAM = mlp.py
+
+# ARGUMENTS =
+
+setup: venv pip_upgrade install
+
+venv:
+	uv venv --python ${PYTHON_VERSION} ${VENV} --seed
+	ln -sf ${ACTIVATE} activate
+
+pip_upgrade:
+	uv pip install --upgrade pip
+
+install: \
+	requirements \
+	# module \
+#
+requirements: requirements.txt
+	uv pip install -r requirements.txt --upgrade
+
+module: setup.py
+	uv pip install -e . --upgrade
+
+list:
+	uv pip list
+
+version:
+	uv python list
+
+size:
+	du -hd 0
+	du -hd 0 ${VENV}
+
+run:
+	${PYTHON} ${EXAMPLES}/${PROGRAM} \
+	# ${ARGUMENTS}
+
+test:
+	PYTHONPATH=. ${PYTHON} ${EXAMPLES}/test.py
+
+clean:
+
+fclean: clean
+	rm -rf ${VENV}
+	rm -rf activate
+
+re: fclean setup run
+
+.SILENT:
+.PHONY: setup venv pip_upgrade install module requirements list version run clean fclean re
---
+++ dlf/Tensor.py
@@ -0,0 +1,111 @@
+import numpy as np
+from enum import auto, IntEnum
+
+class Operations(IntEnum):
+    ADD = auto()
+    MUL = auto()
+    SUM = auto()
+    NEG = auto()
+
+backward_operations = {
+    Operations.ADD: lambda gradient, parent: (gradient, gradient),
+    Operations.MUL: lambda gradient, parents: (parents[1] * gradient, parents[0] * gradient),
+    Operations.SUM: lambda gradient, parent: (np.full_like(parent, gradient)),
+    Operations.NEG: lambda gradient, parents: (None),
+}
+
+class Tensor():
+    def __init__(self, data, name=None):
+        if isinstance(data, np.ndarray):
+            self.data = data
+        else:
+            self.data = np.array(data)
+        self.grad: np.ndarray = None
+        self.context = None
+
+    def topo_sort(self):
+        ret = dict()
+        stack = [(self, False)]
+        while stack:
+            node, visited = stack.pop()
+            if node in ret: continue
+            if not visited:
+                if node.context is not None:
+                    stack.append((node, True))
+                    ops, *parents = node.context
+                    for parent in parents: stack.append((parent, False))
+            else:
+                ret[node] = None
+        return ret
+
+
+    def backward(self):
+        """
+        input: a list of nodes as a paramater (the result from the topo_sort)
+        apply backward from backward_operations[ops] on each nodes
+        """
+
+        operations = []
+        # check before doing backward, scalar variable
+        self.grad = np.array(1)
+
+        # check type before using them in backward_operations
+        # has to be checked at creation actually
+
+        for element in reversed(self.topo_sort()):
+            ops, *parents = element.context
+            # print(element.__dict__['name'], Operations(ops).name, *parents)
+            # print(element, Operations(ops).name)
+            operations.append((element, Operations(ops).name))
+            backward_operation = backward_operations[ops]
+            gradients = backward_operation(element.data, [*parents])
+            # print(f"gradients: {gradients}")
+            # print(f"type gradients: {type(gradients)}")
+            # print(f"\nnew parents")
+            for parent, gradient in zip(parents, gradients):
+                # print(f"gradient: {gradient}")
+                # print(f"parent: {parent}")
+                if parent.grad is None:
+                    grad = gradient
+                else:
+                    grad += gradient
+
+        list_ops = []
+        for operation in operations:
+            tensor, ops = operation
+            list_ops.append(f"{ops} : {tensor.data.shape}")
+
+        result = " ---> ".join(list_ops)
+        print(f"\n\n{result}")
+
+        return
+
+    def __repr__(self):
+        if self.context is not None:
+            print(self.context)
+            ops, *parents = self.context
+            return f"<{self.data.shape}, {self.data}, {ops}>"
+        else:
+            return f"<{self.data.shape}, {self.data}>"
+
+    def __add__(self, x):
+        return self.ADD(x)
+
+    def ADD(self, x):
+        result = Tensor(self.data + x.data)
+        result.context = (Operations.ADD, self, x)
+        # result.context = (Operations.ADD, self, x)
+        return result
+
+    def __mul__(self, x):
+        return self.MUL(x)
+
+    def MUL(self, x):
+        result = Tensor(self.data * x.data)
+        result.context = (Operations.MUL, self, x)
+        return result
+
+    def SUM(self):
+        result = Tensor(np.sum(self.data))
+        result.context = (Operations.SUM, self)
+        return result
