b311305a169ab94969f00590b5e2c43c9ae4af99
plgch <plagache@protonmail.com>
2025-10-01 15:45:07 +0200

    no need for get_parameters;
    we can directly pass them to the optimizer;
    
    we should zero_grad in optimizer since it needs params;

--- dlf/optimizer.py
+++ dlf/optimizer.py
@@ -1,17 +1,11 @@
 from .tensor import Tensor
 
-def get_parameters(model):
-    variables = vars(model)
-    print("variables: ", variables)
-    return variables
+# def get_parameters(model):
+#     variables = vars(model)
+#     print("variables: ", variables)
+#     return variables
 
 
-def zero_grad(params):
-    for param in params:
-        param.grad = None
-        # print("zero grad: ", param)
-        # print("param.grad:", param.grad)
-    return # a model that has None as gradient       return
 
 class SGD():
     def __init__(self, params:list[Tensor], learning_rate: float):
@@ -19,10 +13,15 @@ class SGD():
         self.lr = learning_rate
 
     def step(self):
-        zero_grad(self.params)
         for param in self.params:
             if param.grad is not None:
                 param.data = Tensor(-self.lr) * param.grad
                 print(param.data)
                 # print(param)
-                # print(param.grad)
+ 
+    def zero_grad(self):
+        for param in self.params:
+            param.grad = None
+            # print("zero grad: ", param)
+            # print("param.grad:", param.grad)
+        return # a model that has None as gradient       return               # print(param.grad)
