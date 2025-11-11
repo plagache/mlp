107cb9b8425f60b04e62a9d0bebeaeadd7ebcab6
user <user@host-asus.lan>
2025-11-11 11:38:08 -0500

    create input and target from polars dataset;

--- examples/mlp.py
+++ examples/mlp.py
@@ -10,38 +10,40 @@ random.seed(42)
 # we should look into regularisation
 # Transform M-B in 0-1
 data = pl.read_csv("data.csv", has_header=False)
-print(data.columns)
+# print(data.columns)
 
 data = data.with_columns(
-    pl.col("column_2").replace({"M": 1,"B": 0}).alias("column_2")
+    pl.col("column_2").replace({"M": 1,"B": 0}).cast(pl.Float64).alias("column_2")
 )
-print(data)
-types = data["column_2"].value_counts()
-print(types)
+# print(data)
+# types = data["column_2"].value_counts()
+# print(types)
 
 class Network:
     def __init__(self):
-        self.l1 = Tensor(np.random.rand(4,))
-        self.l2 = Tensor(np.random.rand(4,))
+        self.weight1 = Tensor(np.random.rand(30,))
+        self.bias1 = Tensor(np.random.rand(30,))
 
     def __call__(self, x: Tensor):
-        x = self.l1 * x
-        x = self.l2 + x
-        return x
+        x = self.weight1 * x
+        x = self.bias1 + x
+        return x.SIGMOID()
 
 model = Network()
 
-input = Tensor([7, 9, 5, 3])
+target = Tensor(data["column_2"])
+inputs = Tensor(data.select(data.columns[2:]))
 
 steps = 5
 
-optimizer = SGD([model.l1, model.l2], 0.002)
+optimizer = SGD([model.weight1, model.bias1], 0.002)
 
 for step in range(steps):
     print(step)
     optimizer.zero_grad()
-    resultat = model(input)
+    resultat = model(inputs)
     # loss = 
     resultat.backward()
     optimizer.step()
+    print(model.weight1)
     print(resultat)
