50ac30ac6d6ba369441a1a8a7efb5b311ade5677
plgch <plagache@protonmail.com>
2025-11-24 21:54:31 +0100

    add 2 more layers;
    add precision so that we can test things;

--- examples/mlp.py
+++ examples/mlp.py
@@ -8,7 +8,9 @@ from dlf.nn import Linear
 class Network:
     def __init__(self):
         self.l1 = Linear(30, 10)
-        self.l2 = Linear(10, 30)
+        self.l2 = Linear(10, 10)
+        self.l3 = Linear(10, 10)
+        self.l4 = Linear(10, 2)
         # self.weight1 = Tensor(np.random.uniform(-0.1, 0.1, (30,10)))
         # self.bias1 = Tensor(np.random.uniform(-0.1, 0.1, (10,)))
         # self.weight2 = Tensor(np.random.uniform(-0.1, 0.1, (10,30)))
@@ -17,12 +19,12 @@ class Network:
     def __call__(self, x: Tensor):
         # x = x @ self.weight1
         # x = x + self.bias1
-        x = self.l1(x)
-        x = x.RELU()
-        x = self.l2(x)
         # x = x @ self.weight2
         # x = x + self.bias2
-        x = x.RELU()
+        x = self.l1(x).RELU()
+        x = self.l2(x).RELU()
+        x = self.l3(x).RELU()
+        x = self.l4(x).RELU()
         return x.SOFTMAX()
 
 model = Network()
@@ -32,17 +34,21 @@ X_train, Y_train, X_test, Y_test = load_dataset()
 steps = 5
 
 # optimizer = SGD([model.weight1, model.bias1], 0.002)
-optimizer = SGD([model.l1.weight, model.l1.bias, model.l2.weight, model.l2.bias], 0.002)
+# optimizer = SGD([model.l1.weight, model.l1.bias, model.l2.weight, model.l2.bias], 0.002)
 
 for step in range(steps):
     print(step)
-    optimizer.zero_grad()
+    # optimizer.zero_grad()
     # print(Tensor(X_train).data)
     # print(X_train)
     resultat = model(Tensor(X_train))
+    probability = np.argmax(resultat.data, -1)
+    # precision = ()
+    # (y_prediction == y_true).sum() / len(y_true)
     # loss = 
     # resultat.backward()
     # optimizer.step()
     # print(model.weight1)
-    print(resultat)
-    print(type(resultat))
+    print(f"{resultat=}, {type(resultat)}")
+    print(f"{probability=}")
+    # print(type(resultat))
--- examples/tiny.py
+++ examples/tiny.py
@@ -1,3 +1,4 @@
+import numpy as np
 from tinygrad import Tensor, nn
 from dlf.dataset import load_dataset
 from tinygrad import Device
@@ -6,12 +7,14 @@ print(Device.DEFAULT)
 class Network:
     def __init__(self):
         self.l1 = nn.Linear(30, 10)
-        self.l2 = nn.Linear(10, 30)
+        self.l2 = nn.Linear(10, 10)
+        self.l3 = nn.Linear(10, 10)
+        self.l4 = nn.Linear(10, 2)
     def __call__(self, x:Tensor) -> Tensor:
-        x = self.l1(x)
-        x = x.relu()
-        x = self.l2(x)
-        x = x.relu()
+        x = self.l1(x).relu()
+        x = self.l2(x).relu()
+        x = self.l3(x).relu()
+        x = self.l4(x).relu()
         return x.softmax()
 
 model = Network()
@@ -23,6 +26,8 @@ for step in range(steps):
     print(step)
     resultat = model(Tensor(X_train.to_numpy()))
     values = resultat.numpy()
+    probability = np.argmax(values, -1)
     # print(resultat)
     print(values.tolist())
+    print(f"{probability=}")
     # print(type(resultat))
