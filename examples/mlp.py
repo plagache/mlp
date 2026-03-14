41cee535be90878996e630f88c16a2cdf5ff2073
plagache <plagache@protonmail.com>
2026-03-14 19:03:40 +0100

    enable optimizer;
    remove unused;

--- examples/mlp.py
+++ examples/mlp.py
@@ -12,16 +12,8 @@ class Network:
         self.l2 = Linear(10, 10)
         self.l3 = Linear(10, 10)
         self.l4 = Linear(10, 2)
-        # self.weight1 = Tensor(np.random.uniform(-0.1, 0.1, (30,10)))
-        # self.bias1 = Tensor(np.random.uniform(-0.1, 0.1, (10,)))
-        # self.weight2 = Tensor(np.random.uniform(-0.1, 0.1, (10,30)))
-        # self.bias2 = Tensor(np.random.uniform(-0.1, 0.1, (30,)))
 
     def __call__(self, x: Tensor):
-        # x = x @ self.weight1
-        # x = x + self.bias1
-        # x = x @ self.weight2
-        # x = x + self.bias2
         x = self.l1(x).RELU()
         x = self.l2(x).RELU()
         x = self.l3(x).RELU()
@@ -32,13 +24,10 @@ class Network:
 model = Network()
 
 X_train, Y_train, X_test, Y_test = load_dataset()
-# for the log_loss to work i need to rewrite the encoding of the Y_s
-# create a tensor with one column per classes
 
 steps = 5
 
-# optimizer = SGD([model.weight1, model.bias1], 0.002)
-# optimizer = SGD([model.l1.weight, model.l1.bias, model.l2.weight, model.l2.bias], 0.002)
+optimizer = SGD([model.l1.weight, model.l1.bias, model.l2.weight, model.l2.bias], 0.002)
 
 
 # def accuracy(true, prediction):
@@ -65,5 +54,5 @@ for step in range(steps):
 
     loss_val.backward()
 
-    # optimizer.step()
-    # print(model.weight1)
+    optimizer.step()
+    print(model.l1.weight)
