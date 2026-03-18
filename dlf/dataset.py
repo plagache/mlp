8a00d8e413fa313ddfab7971d85af01d8f736c43
plagache <plagache@protonmail.com>
2026-03-18 08:57:04 +0100

    add normalization;
    does it explode ?

--- dlf/dataset.py
+++ dlf/dataset.py
@@ -3,7 +3,9 @@ import polars as pl
 def load_dataset():
     """
     there is no missing value
-    we should look into regularisation
+    we should look into regularisation / normalization
+
+    need mean and variance calc for all features
     """
     data = pl.read_csv("data.csv", has_header=False)
 
@@ -17,4 +19,15 @@ def load_dataset():
 
     frac = int(0.8 * data_len)
 
-    return X[:frac], Y[:frac], X[frac:], Y[frac:]
+    X_train = X[:frac]
+    Y_train = Y[:frac]
+    X_test = X[frac:]
+    Y_test = Y[frac:]
+
+    mean_train = X_train.mean()
+    std_train = X_train.std()
+
+    X_train_norm = (X_train - mean_train) / std_train
+    X_test_norm = (X_test - mean_train) / std_train
+
+    return X_train_norm, Y_train, X_test_norm, Y_test
