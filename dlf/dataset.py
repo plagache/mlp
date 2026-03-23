fix normalization;
compute a mean and std for each features;

dlf/dataset.py
@@ -1,16 +1,11 @@
 import polars as pl
 
-def load_dataset():
-    """
-    there is no missing value
-    we should look into regularisation / normalization
 
-    need mean and variance calc for all features
-    """
+def load_dataset():
     data = pl.read_csv("data.csv", has_header=False)
 
-    data = data.with_columns(pl.col("column_2").replace({"M": 1,"B": 0}).cast(pl.Float64).alias("Malign"))
-    data = data.with_columns(pl.col("column_2").replace({"M": 0,"B": 1}).cast(pl.Float64).alias("Benign"))
+    data = data.with_columns(pl.col("column_2").replace({"M": 1, "B": 0}).cast(pl.Float64).alias("Malign"))
+    data = data.with_columns(pl.col("column_2").replace({"M": 0, "B": 1}).cast(pl.Float64).alias("Benign"))
 
     Y = data.select(["Malign", "Benign"])
     X = data.select(data.columns[2:32])
@@ -19,13 +14,14 @@ def load_dataset():
 
     frac = int(0.8 * data_len)
 
-    X_train = X[:frac]
-    Y_train = Y[:frac]
-    X_test = X[frac:]
-    Y_test = Y[frac:]
+    X_train = X[:frac].to_numpy()
+    Y_train = Y[:frac].to_numpy()
+    X_test = X[frac:].to_numpy()
+    Y_test = Y[frac:].to_numpy()
 
-    mean_train = X_train.mean()
-    std_train = X_train.std()
+    # axis=0 so we have a mean for each features (30,) and not THE MEAN and a reduce axis ()
+    mean_train = X_train.mean(axis=0)
+    std_train = X_train.std(axis=0)
 
     X_train_norm = (X_train - mean_train) / std_train
     X_test_norm = (X_test - mean_train) / std_train