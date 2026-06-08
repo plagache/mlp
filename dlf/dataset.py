fda8ab593d3af72cf1e37e4c6726a216eb2e3959
plagache <plagache@protonmail.com>
2026-06-08 09:32:14 +0200

    add shuffle and keep logic from split into create_data;
    remove frac from load_dataset;
    read train and valid;

--- dlf/dataset.py
+++ dlf/dataset.py
@@ -1,4 +1,5 @@
 import os
+# import Path
 
 import numpy as np
 import polars as pl
@@ -10,25 +11,49 @@ def compute_accuracy(targets: np.ndarray, predictions: np.ndarray) -> float:
     return np.mean(predictions_classes == targets_classes) * 100
 
 
-def load_dataset():
+def create_data(percent=0.8, shuffle=True, seed=None):
+
+    # no need for the function
+    if os.path.exists("data_train.csv") and os.path.exists("data_valid.csv"):
+        data_train = pl.read_csv("data_train.csv", has_header=False)
+        data_valid = pl.read_csv("data_valid.csv", has_header=False)
+        print(f"data already split")
+        return
+
+    # cannot do the function
     assert os.path.exists("data.csv"), "data.csv not found"
     data = pl.read_csv("data.csv", has_header=False)
-    print(data)
-
-    data = data.with_columns(pl.col("column_2").replace({"M": 1, "B": 0}).cast(pl.Float64).alias("Malign"))
-    data = data.with_columns(pl.col("column_2").replace({"M": 0, "B": 1}).cast(pl.Float64).alias("Benign"))
-
-    Y = data.select(["Malign", "Benign"])
-    X = data.select(data.columns[2:32])
+    data.sample(fraction=percent, shuffle=shuffle, seed=seed)
+    print(f"data being split:\n{data}")
 
     data_len = len(data)
 
-    frac = int(0.8 * data_len)
+    frac = int(percent * data_len)
 
-    X_train = X[:frac].to_numpy()
-    Y_train = Y[:frac].to_numpy()
-    X_test = X[frac:].to_numpy()
-    Y_test = Y[frac:].to_numpy()
+    train = data[:frac]
+    valid = data[frac:]
+    train.write_csv("data_train.csv", include_header=False)
+    valid.write_csv("data_valid.csv", include_header=False)
+    return
+
+
+def load_dataset():
+    if not os.path.exists("data_train.csv") or os.path.exists("data_valid.csv"):
+        create_data()
+    data_train = pl.read_csv("data_train.csv", has_header=False)
+    data_valid = pl.read_csv("data_valid.csv", has_header=False)
+    # print(data_train)
+    # print(data_valid)
+
+    data_train = data_train.with_columns(pl.col("column_2").replace({"M": 1, "B": 0}).cast(pl.Float64).alias("Malign"))
+    data_train = data_train.with_columns(pl.col("column_2").replace({"M": 0, "B": 1}).cast(pl.Float64).alias("Benign"))
+    data_valid = data_valid.with_columns(pl.col("column_2").replace({"M": 1, "B": 0}).cast(pl.Float64).alias("Malign"))
+    data_valid = data_valid.with_columns(pl.col("column_2").replace({"M": 0, "B": 1}).cast(pl.Float64).alias("Benign"))
+
+    Y_train = data_train.select(["Malign", "Benign"]).to_numpy()
+    X_train = data_train.select(data_train.columns[2:32]).to_numpy()
+    Y_test = data_valid.select(["Malign", "Benign"]).to_numpy()
+    X_test = data_valid.select(data_valid.columns[2:32]).to_numpy()
 
     # axis=0 so we have a mean for each features (30,) and not THE MEAN and a reduce axis ()
     mean_train = X_train.mean(axis=0)
@@ -40,33 +65,6 @@ def load_dataset():
     return X_train_norm, Y_train, X_test_norm, Y_test
 
 
-def create_data():
-
-    if os.path.exists("data_train.csv") and os.path.exists("data_valid.csv"):
-        data_train = pl.read_csv("data_train.csv", has_header=False)
-        data_valid = pl.read_csv("data_valid.csv", has_header=False)
-        print(data_train)
-        print(data_valid)
-        print(f"data already split")
-        return
-
-    assert os.path.exists("data.csv"), "data.csv not found"
-    data = pl.read_csv("data.csv", has_header=False)
-    print(data)
-
-    data_len = len(data)
-
-    frac = int(0.8 * data_len)
-
-    train = data[:frac]
-    valid = data[frac:]
-    train.write_csv("data_train.csv")
-    valid.write_csv("data_valid.csv")
-    # print(train)
-    # print(valid)
-    return
-
-
 if __name__ == "__main__":
-    # load_dataset()
-    create_data()
+    X_train, Y_train, X_test, Y_test = load_dataset()
+    print(f"{X_train.shape=}\n{Y_train.shape=}\n{X_test.shape=}\n{Y_test.shape=}")
